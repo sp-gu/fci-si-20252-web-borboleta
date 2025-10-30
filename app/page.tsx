9c443f4a1cc60340b3a1d7b44cd728cb049b12ae
Gustavo Sperandio <sp_gu@outlook.com>
2025-10-30 19:27:55 -0300

    added new proj to the repo

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,56 @@
 import Image from "next/image";
+import Header from "./components/Header";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
+    <body>
+    <Header />
+
+    <section className="topo-site"> 
+        <h1>Conheça o bazar Heleninha</h1>
+        <h2>Bazar beneficente</h2>
+     
+        <button id="saiba-mais">
+            <a href="#">Saiba mais!</a>
+        </button>
+    </section>
+
+    <section className="produtos-destaque">
+        <h1>Produtos em Destaque</h1>
+        <article id="produtos">
+            
+        </article>
+    </section>
+
+    <section className="parallax"> </section>
+
+    <section className="mvv">
+        <article className="missao">
+            <h2>Missão</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod  .</p>
+        </article>
+        <article className="visao">
+            <h2>Visão</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.</p>
+        </article>
+        <article className="valores">
+            <h2>Valores</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.</p>
+        </article>
+    </section>
+
+    <footer>
+        <section className="rodape">
+            <article className="left-side">
+                <h4>Redes sociais:</h4>
+                <a href="https://www.instagram.com/bazar.heleninha?igsh=MTRmM3lyMXVzdnQ0ag=="></a>
+            </article>
+            <article className="right-side">
+                <h4>Horários: </h4>
+                <p>Seg. a Sex. das 10h às 16h</p>
+            </article>
+        </section>
+    </footer>
+</body>
   );
 }
